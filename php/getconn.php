acbc4e7aa231efca41ae2acdc0e49a291e5ffe64
Robin Stroobants <robin@webbit.be>
2026-04-18 23:38:53 +0200

    chore: remove secrets from repository and switch to env configuration

--- php/getconn.php
+++ php/getconn.php
@@ -2,7 +2,7 @@
 $host = getenv('DB_HOST') ?: 'db';
 $db = getenv('DB_NAME') ?: 'lineup_db';
 $user = getenv('DB_USER') ?: 'app_user';
-$pass = getenv('DB_PASS') ?: 'bRng4y8TJLJwUxYHBD6q';
+$pass = getenv('DB_PASS') ?: '';
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);

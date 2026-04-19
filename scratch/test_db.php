e4da4a792c3f080848aab93d4ca8d7899ca51ccf
Robin Stroobants <robin@webbit.be>
2026-04-20 00:41:04 +0200

    Chore: cleanup obsolete DB scripts, dead sqlite file, and scratch folders

--- scratch/test_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once '/home/customer/www/lineup.webbit.be/public_html/voetbal/php/getconn.php';
-$res = $conn->query("SHOW PROCESSLIST");
-while($row = $res->fetch_assoc()) {
-    print_r($row);
-}
